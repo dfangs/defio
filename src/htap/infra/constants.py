8d1a5d0265b5befd34c549c7ad9c7c608014db9a
dfangs <dfanggohans@gmail.com>
2023-04-12 01:37:28 -0400

    Implement and test Pulumi utility functions

---
+++ src/htap/infra/constants.py
@@ -0,0 +1,5 @@
+import ipaddress
+
+PACKAGE_NAME = "htap"  # Used by `ComponentMixin`
+
+ALL_NETWORK = ipaddress.IPv4Network("0.0.0.0/0")
---
+++ tests/infra/test_utils.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+import pytest
+from pytest_mock import MockerFixture
+
+from htap.infra.constants import PACKAGE_NAME
+from htap.infra.utils import ComponentMixin, get_az
+
+
+def test_component_mixin() -> None:
+    class SomeResource(ComponentMixin):
+        ...
+
+    assert (
+        SomeResource().get_type_name()
+        == f"{PACKAGE_NAME}:{Path(__file__).stem}:{SomeResource.__name__}"
+    )
+
+
+@pytest.fixture(name="_mock_azs")
+def fixture_mock_get_available_azs(mocker: MockerFixture) -> None:
+    # Note: For simplicity, only mock the `get_available_azs()` function
+    # Mocking the whole `pulumi_aws` would be slower and rather unnecessary
+    # (but as a consequence, we don't test the other helper functions)
+    mocker.patch(
+        "htap.infra.utils.get_available_azs",
+        return_value=["us-east-1a", "us-east-1b"],
+    )
+
+
+@pytest.mark.parametrize(
+    "index, wrap_around, expected",
+    [
+        pytest.param(0, False, "us-east-1a"),
+        pytest.param(1, False, "us-east-1b"),
+        pytest.param(-1, True, "us-east-1b"),
+        pytest.param(0, True, "us-east-1a"),
+        pytest.param(1, True, "us-east-1b"),
+        pytest.param(2, True, "us-east-1a"),
+    ],
+)
+def test_get_az_ok(_mock_azs, index: int, wrap_around: bool, expected: str) -> None:
+    assert get_az(index, wrap_around) == expected
+
+
+@pytest.mark.parametrize(
+    "index, wrap_around",
+    [
+        pytest.param(-1, False),
+        pytest.param(2, False),
+    ],
+)
+def test_get_az_invalid_index(_mock_azs, index: int, wrap_around: bool) -> None:
+    with pytest.raises(IndexError):
+        get_az(index, wrap_around)
