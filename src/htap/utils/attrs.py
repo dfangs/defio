56ba5ac00cc368c21115785eeee54dba1dc60a5f
dfangs <dfanggohans@gmail.com>
2023-04-28 17:57:55 -0400

    Improve the documentation for `attrs` utils

--- src/htap/utils/attrs.py
+++ src/htap/utils/attrs.py
@@ -6,8 +6,15 @@ _T = TypeVar("_T")
 _U = TypeVar("_U")
 
 
-# NOTE: Only handle lists; don't use sequence (e.g., str is also a sequence)
 def to_tuple(arg: list[_T] | _U) -> tuple[_T] | _U:
+    """
+    Converts the argument into tuple if it is a list instance,
+    or otherwise simply returns its unchanged.
+
+    This function should only be used as a converter in `attrs.field`.
+
+    NOTE: Only handle lists; do not handle all `Sequence` (e.g., `str`).
+    """
     if isinstance(arg, list):
         return tuple(arg)
     return arg
@@ -19,6 +26,14 @@ _V = TypeVar("_V")
 
 # Same reasoning as above
 def to_map(arg: dict[_K, _V] | _U) -> Map[_K, _V] | _U:
+    """
+    Converts the argument into `immutables.Map` if it is a dict instance,
+    or otherwise simply returns its unchanged.
+
+    This function should only be used as a converter in `attrs.field`.
+
+    NOTE: Only handle dicts; do not handle all `Mapping`.
+    """
     if isinstance(arg, dict):
         return Map(arg)
     return arg
