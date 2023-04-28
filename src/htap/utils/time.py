27e762e3b8765def4fc26cdd5e19f87300c3e01e
dfangs <dfanggohans@gmail.com>
2023-04-28 17:56:59 -0400

    Improve the interface of `time` utils
    Other changes:
    - Add a full test suite for time utils

--- src/htap/utils/time.py
+++ src/htap/utils/time.py
@@ -1,47 +1,125 @@
+from __future__ import annotations
+
 import asyncio
 import time
-from collections.abc import Iterator
+from collections.abc import Callable, Iterator
 from contextlib import contextmanager
-from datetime import UTC, datetime, timedelta
 from typing import final
 
-from attrs import define, field
+import pendulum
+from attrs import define
+from pendulum import UTC, DateTime, Period  # type: ignore
+
+_SECONDS_TO_MICROSECONDS = 1_000_000
 
 
 @final
 @define
 class TimeMeasurement:
-    start_time: datetime
-    _elapsed_time: timedelta | None = field(default=None, init=False)
+    """
+    Represents a measurement of some time interval up to microsecond resolution.
+    """
+
+    start_time: DateTime
+    _start_time_benchmark: float
+    _end_time_benchmark: float | None
+    _timer: Callable[[], float]
+
+    def __init__(
+        self, /, *, start_time: DateTime, timer: Callable[[], float] = time.perf_counter
+    ) -> None:
+        # Get actual time from the input
+        self.start_time = start_time
+
+        # Use timer function (e.g., `perf_counter()`) for more accurate duration
+        self._start_time_benchmark = timer()
+        self._end_time_benchmark = None
+
+        # Make this customizable, in case a different level of precision is desired
+        self._timer = timer
+
+    @staticmethod
+    def start(timer: Callable[[], float] = time.perf_counter) -> TimeMeasurement:
+        """Starts a new time measurement now."""
+        return TimeMeasurement(start_time=get_current_time(), timer=timer)
+
+    def stop(self) -> None:
+        """Stops this time measurement and records the end time."""
+        self._end_time_benchmark = self._timer()
 
     @property
-    def elapsed_time(self) -> timedelta:
-        if self._elapsed_time is None:
+    def end_time(self) -> DateTime:
+        """
+        Returns the end time of this measurement.
+
+        Raises a `ValueError` if the measurement has not finished.
+        """
+        if self._end_time_benchmark is None:
             raise ValueError("Measurement has not finished yet")
-        return self._elapsed_time
+
+        return self.start_time.add(
+            microseconds=int(
+                _SECONDS_TO_MICROSECONDS
+                * (self._end_time_benchmark - self._start_time_benchmark)
+            )
+        )
+
+    @property
+    def elapsed_time(self) -> Period:
+        """
+        Returns the elapsed time between the start time and end time
+        of this time measurement.
+
+        Raises a `ValueError` if the measurement has not finished.
+        """
+        return self.end_time - self.start_time
+
+    @property
+    def total_seconds(self) -> float:
+        """
+        Returns the total seconds of the elapsed time of this measurement.
+
+        Raises a `ValueError` if the measurement has not finished.
+        """
+        return self.elapsed_time.total_seconds()
 
 
 @contextmanager
-def measure_time() -> Iterator[TimeMeasurement]:
-    # Use `datetime.now()` to get actual time
-    measurement = TimeMeasurement(start_time=get_current_time())
+def measure_time(
+    timer: Callable[[], float] = time.perf_counter
+) -> Iterator[TimeMeasurement]:
+    """
+    Context manager that measures the elapsed time
+    over the code block.
 
-    # Use `perf_counter()` for more accurate duration
-    start_time = time.perf_counter()
+    Usage:
+    ```
+    with measure_time() as measurement:
+        ...  # Do some work here
 
+    # Get measurement
+    print(measurement.elapsed_time)
+    print(f"{measurement.total_seconds:.2f} seconds")
+    ```
+    """
+    measurement = TimeMeasurement.start(timer)
     try:
         yield measurement
-
     finally:
-        # Compute elapsed time on exit
-        measurement._elapsed_time = timedelta(  # pylint: disable=protected-access
-            seconds=time.perf_counter() - start_time
-        )
+        measurement.stop()
 
 
-def get_current_time() -> datetime:
-    return datetime.now(tz=UTC)
+def get_current_time() -> DateTime:
+    """Returns the current time in UTC."""
+    return pendulum.now(tz=UTC)
 
 
 def get_event_loop_time() -> float:
+    """
+    Returns the current internal time of the running event loop.
+
+    In general, the returned value should be monotonic, and thus
+    more appropriate for computing time intervals in the context
+    of asyncio.
+    """
     return asyncio.get_running_loop().time()
---
+++ tests/htap/utils/test_time.py
@@ -0,0 +1,92 @@
+import time
+from unittest.mock import MagicMock
+
+import pendulum
+import pytest
+from pytest_mock import MockerFixture
+
+from htap.utils.time import TimeMeasurement, measure_time
+
+CURRENT_TIME = pendulum.datetime(year=2023, month=3, day=12)
+SECONDS_TO_MICROSECONDS = 1_000_000
+
+
+@pytest.fixture(name="_mock_current_time")
+def fixture_mock_current_time(mocker: MockerFixture) -> None:
+    mocker.patch("htap.utils.time.get_current_time", return_value=CURRENT_TIME)
+
+
+@pytest.fixture(name="mock_timer")
+def fixture_mock_timer(mocker: MockerFixture) -> MagicMock:
+    return mocker.spy(time, "perf_counter")
+
+
+class TestTimeMeasurement:
+    def test_start_time(self, _mock_current_time) -> None:
+        measurement = TimeMeasurement.start()
+        assert measurement.start_time == CURRENT_TIME
+
+    def test_end_time(self, _mock_current_time, mock_timer: MagicMock) -> None:
+        measurement = TimeMeasurement.start(timer=mock_timer)
+        start_time_benchmark = mock_timer.spy_return
+
+        measurement.stop()
+        end_time_benchmark = mock_timer.spy_return
+
+        # End time should be measured using the given `timer()` function
+        # up to microsecond resolution
+        expected_end_time = CURRENT_TIME.add(
+            microseconds=int(
+                (end_time_benchmark - start_time_benchmark) * SECONDS_TO_MICROSECONDS
+            )
+        )
+        assert measurement.end_time == expected_end_time
+
+    def test_elapsed_time(self) -> None:
+        measurement = TimeMeasurement.start()
+        measurement.stop()
+
+        assert measurement.elapsed_time == measurement.end_time - measurement.start_time
+
+    def test_total_seconds(self) -> None:
+        measurement = TimeMeasurement.start()
+        measurement.stop()
+
+        assert measurement.total_seconds == measurement.elapsed_time.total_seconds()
+
+    def test_unfinished(self) -> None:
+        measurement = TimeMeasurement.start()
+
+        with pytest.raises(ValueError):
+            print(measurement.end_time)
+
+        with pytest.raises(ValueError):
+            print(measurement.elapsed_time)
+
+        with pytest.raises(ValueError):
+            print(measurement.total_seconds)
+
+
+class TestMeasureTime:
+    def test_ok(self, _mock_current_time, mock_timer: MagicMock) -> None:
+        with measure_time(timer=mock_timer) as measurement:
+            # `timer()` should be called once before entering the context...
+            start_time_benchmark = mock_timer.spy_return
+
+        # ... and once after exiting the context
+        end_time_benchmark = mock_timer.spy_return
+
+        assert measurement.start_time == CURRENT_TIME
+
+        expected_end_time = CURRENT_TIME.add(
+            microseconds=int(
+                (end_time_benchmark - start_time_benchmark) * SECONDS_TO_MICROSECONDS
+            )
+        )
+        assert measurement.end_time == expected_end_time
+
+    def test_bad(self) -> None:
+        with pytest.raises(ValueError):
+            with measure_time() as measurement:
+                # Must not get the elapsed time within the block
+                print(measurement.elapsed_time)
