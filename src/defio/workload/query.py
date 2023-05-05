94cd353f954199f48dd60f9244a1d9ccf2d761b1
dfangs <dfanggohans@gmail.com>
2023-05-04 22:42:12 -0400

    Improve the design of `Query`-related classes
    Additional changes:
    - Simplify `QueryGenerator` (now it no longer requires `ScheduleGenerator`
    - Add unit tests

--- src/defio/workload/query.py
+++ src/defio/workload/query.py
@@ -2,15 +2,15 @@ from __future__ import annotations
 
 from collections.abc import Iterator, Sequence
 from datetime import datetime, timedelta
-from typing import Any, TypeAlias, final
+from typing import Any, Generic, TypeAlias, TypeVar, final
 
 from attrs import define, field
 from typing_extensions import override
 
 from defio.sqlgen.generator import SqlGenerator
-from defio.utils.attrs import to_tuple
 from defio.utils.generator import ImmutableGenerator
-from defio.workload.schedule import Schedule, ScheduleGenerator
+from defio.utils.time import get_current_time
+from defio.workload.schedule import Once, Schedule
 from defio.workload.user import User
 
 
@@ -20,122 +20,176 @@ class Query:
     """
     Represents a SQL query with an execution schedule.
 
-    In the context of a workload runner, a `Query` goes through
+    In the context of a workload runner, a query goes through
     the following lifecycle:
     - `INITIAL`: The query has not been processed by the runner yet.
-    - `STARTED`: The .
-    - `PENDING`: The runner has put the query into its internal queue.
-    - `RUNNING`: The query has been sent to some database engine.
-    - `COMPLETED`: The query has finished its execution.
+    - `STARTED`: The query has started being processed the runner.
+    - `SCHEDULED`: The query has been scheduled to run by the runner.
+    - `RUNNING`: The query has been executed on some database engine.
+    - `COMPLETED`: The query has completed its execution schedule.
+
+    Note that for a recurring query (i.e. query with a `Repeat` schedule),
+    it only reaches the `COMPLETED` state after the entire execution
+    schedule has completed. During its execution period, its state will
+    switch back and forth between `SCHEDULED` and `RUNNING`.
     """
 
     sql: str
     schedule: Schedule
 
+    def start(
+        self, /, *, user: User, processed_time: datetime, scheduled_time: datetime
+    ) -> ScheduledQuery:
+        """Schedules this query to be executed at least once in the future."""
+        return ScheduledQuery(
+            user=user,
+            query=self,
+            processed_time=processed_time,
+            scheduled_time=scheduled_time,
+        )
+
 
 @final
-@define(frozen=True)
-class PendingQuery:
+@define(frozen=True, kw_only=True)
+class ScheduledQuery:
     """
-    Represents a `Query` that has been scheduled by a workload runner
-    to run at some time(s) in the future, but has not been executed yet
-    (i.e. in the `PENDING` state).
+    Represents a user query that has been scheduled by a workload runner
+    to run _once_ at some time in the future (i.e. it is in the `SCHEDULED`
+    state).
+
+    A single `Query` can be scheduled multiple times by the same user
+    (e.g., with a `Repeat` schedule) or different users.
 
     Definitions:
     - `processed_time` is the time when the query transitions from
-      `INITIAL` to `STARTED`.
+      some other state (e.g., `STARTED` or `RUNNING`) to `SCHEDULED`.
+    - `scheduled_time` is the time when the query is scheduled to
+      be executed on some database.
     """
 
     user: User
     query: Query
     processed_time: datetime
+    scheduled_time: datetime
 
-    def mark_complete(
-        self, result: Sequence[Any], executed_time: datetime, execution_time: timedelta
-    ) -> CompletedQuery:
+    def create_report(
+        self,
+        /,
+        *,
+        executed_time: datetime,
+        execution_time: timedelta,
+        results: Sequence[Any],
+    ) -> QueryReport:
         """Converts this query into a `CompletedQuery`."""
-        return CompletedQuery(
+        return QueryReport(
             user=self.user,
             query=self.query,
-            result=result,
             processed_time=self.processed_time,
+            scheduled_time=self.scheduled_time,
             executed_time=executed_time,
             execution_time=execution_time,
+            results=results,
         )
 
 
+_T = TypeVar("_T")
+
+
 @final
-@define(frozen=True)
-class CompletedQuery:
+@define(frozen=True, eq=False, kw_only=True)
+class QueryReport(Generic[_T]):
     """
-    Represents a `Query` that has completed its execution
-    (i.e. in the `COMPLETED` state).
+    Represents a single execution of a scheduled user query.
+    This contains some information useful for reporting purposes.
 
     Definitions:
     - `executed_time` is the time when the query transitions from
-      `PENDING` to `RUNNING`.
+      `SCHEDULED` to `RUNNING`.
     - `completed_time` is the time when the query transitions from
       `RUNNING` to `COMPLETED`.
-    - `execution_time` is the duration between `executed_time` and
+    - `execution_time` is the duration from `executed_time` to
       `completed_time`.
     """
 
     user: User
     query: Query
-    result: Sequence[Any] = field(converter=to_tuple)
     processed_time: datetime
+    scheduled_time: datetime
     executed_time: datetime
     execution_time: timedelta
+    results: Sequence[_T]
 
     @property
     def completed_time(self) -> datetime:
         return self.executed_time + self.execution_time
 
 
+# Avoid using `Iterable[Query]` since it complicates immutable design.
+# Specifically, we can't simply defensive copy all iterables--what if they're unbounded?
+QuerySource: TypeAlias = Sequence[Query] | ImmutableGenerator[Query]
+
+
 @final
 @define(frozen=True)
 class QueryGenerator(ImmutableGenerator[Query]):
     """
-    Immutable generator that yields a (potentially unbounded) sequence of queries.
+    Immutable generator that yields a (potentially unbounded) sequence
+    of queries.
+
+    This class is intended to convert a `SqlGenerator` (which yields SQL
+    strings) into a generator of full-fledged `Query` objects. As such,
+    the attached `Schedule` should only be instances of `Once`, since
+    `SqlGenerator` shouldn't be used to generate recurring queries.
     """
 
-    sql_generator: SqlGenerator
-    schedule_generator: ScheduleGenerator
+    _sql_generator: SqlGenerator = field(alias="sql_generator")
+    _fixed_time: Once | None = field(default=None, alias="fixed_time")
+    _interval: timedelta | None = field(default=None, alias="interval")
+
+    def __attrs_post_init__(self) -> None:
+        assert sum([self._fixed_time is None, self._interval is None]) == 1
 
     @override
     def __iter__(self) -> Iterator[Query]:
-        for sql, schedule in zip(self.sql_generator, self.schedule_generator):
-            yield Query(sql, schedule)
+        if self._fixed_time is not None:
+            for sql in self._sql_generator:
+                yield Query(sql, self._fixed_time)
+            return
 
+        assert self._interval is not None
 
-# Avoid using `Iterable[Query]` since it complicates immutable design.
-# Specifically, we can't simply defensive copy all iterables--what if they're unbounded?
-Queries: TypeAlias = Sequence[Query] | ImmutableGenerator[Query]
+        start_time = get_current_time()
+        for i, sql in enumerate(self._sql_generator):
+            yield Query(sql, Once(start_time + i * self._interval))
 
+    @staticmethod
+    def with_fixed_time(sql_generator: SqlGenerator, schedule: Once) -> QueryGenerator:
+        """
+        Creates a query generator that yields queries with the given
+        fixed schedule.
+        """
+        return QueryGenerator(sql_generator=sql_generator, fixed_time=schedule)
 
-@final
-@define(frozen=True)
-class ChainedQueries(ImmutableGenerator[Query]):
-    """
-    Represents an immutable `Queries` instance that consists of multiple
-    subqueries.
+    @staticmethod
+    def with_fixed_interval(
+        sql_generator: SqlGenerator, interval: timedelta
+    ) -> QueryGenerator:
+        """
+        Creates a query generator that yields queries scheduled at
+        evenly-spaced interval starting from the first yield.
+        """
+        return QueryGenerator(sql_generator=sql_generator, interval=interval)
 
-    This class is intended to mimic the behavior of `itertools.chain()`,
-    except that we need to implement our own chaining mechanism due to
-    using `ImmutableGenerator` instead of normal iterables.
-    """
-
-    _all_queries: Sequence[Queries]
-
-    def __init__(self, *all_queries: Queries) -> None:
-        # Can't use direct assignment due to frozen dataclass
-        object.__setattr__(
-            self,
-            "_all_queries",
-            tuple(to_tuple(queries) for queries in all_queries),  # Defensive copy
+    @staticmethod
+    def with_fixed_rate(
+        sql_generator: SqlGenerator, queries_per_second: float
+    ) -> QueryGenerator:
+        """
+        Creates a query generator that yields queries with evenly-spaced
+        schedules starting from the first yield, such that no more than
+        `queries_per_second` queries are scheduled within a one-second interval.
+        """
+        return QueryGenerator(
+            sql_generator=sql_generator,
+            interval=timedelta(seconds=1) / queries_per_second,
         )
-
-    @override
-    def __iter__(self) -> Iterator[Query]:
-        for queries in self._all_queries:
-            yield from queries
---
+++ tests/workload/test_query.py
@@ -0,0 +1,117 @@
+from collections.abc import Iterator
+from datetime import datetime, timedelta
+from itertools import pairwise, repeat
+from typing import Final, final
+
+from attrs import define
+
+from defio.sqlgen.generator import SqlGenerator
+from defio.workload.query import Query, QueryGenerator
+from defio.workload.schedule import Once
+from defio.workload.user import User
+
+
+def test_query() -> None:
+    query = Query(
+        sql := "SELECT 1;",
+        schedule := Once(at=datetime(year=2023, month=5, day=2)),
+    )
+
+    assert query.sql == sql
+    assert query.schedule == schedule
+
+    scheduled_query = query.start(
+        user=(user := User.random()),
+        processed_time=(processed_time := schedule.at - timedelta(seconds=1)),
+        scheduled_time=(scheduled_time := schedule.at + timedelta(seconds=0.1)),
+    )
+
+    assert scheduled_query.user == user
+    assert scheduled_query.query == query
+    assert scheduled_query.processed_time == processed_time
+    assert scheduled_query.scheduled_time == scheduled_time
+
+    query_report = scheduled_query.create_report(
+        executed_time=(executed_time := scheduled_time + timedelta(seconds=0.1)),
+        execution_time=(execution_time := timedelta(milliseconds=10)),
+        results=[1],
+    )
+
+    assert query_report.user == user
+    assert query_report.query == query
+    assert query_report.processed_time == processed_time
+    assert query_report.scheduled_time == scheduled_time
+    assert query_report.executed_time == executed_time
+    assert query_report.execution_time == execution_time
+    assert query_report.completed_time == executed_time + execution_time
+
+
+SQL_TEMPLATE = "SELECT {i};"
+NUM_ITERS: Final = 3
+
+
+@final
+@define(frozen=True)
+class DummySqlGenerator(SqlGenerator):
+    num_items: int
+
+    def __iter__(self) -> Iterator[str]:
+        for i in range(self.num_items):
+            yield SQL_TEMPLATE.format(i=i)
+
+
+class TestQueryGenerator:
+    def test_fixed_time(self) -> None:
+        sql_generator = DummySqlGenerator(num_items := 10)
+        schedule = Once(at=datetime(year=2023, month=5, day=2))
+
+        query_generator = QueryGenerator.with_fixed_time(sql_generator, schedule)
+
+        # Query generators can be iterated multiple times
+        for _ in range(NUM_ITERS):
+            assert list(query_generator) == [
+                Query(SQL_TEMPLATE.format(i=i), schedule) for i in range(num_items)
+            ]
+
+    def test_fixed_interval(self) -> None:
+        sql_generator = DummySqlGenerator(num_items := 10)
+        interval = timedelta(seconds=0.5)
+
+        query_generator = QueryGenerator.with_fixed_interval(sql_generator, interval)
+
+        # Query generators can be iterated multiple times
+        for _ in range(NUM_ITERS):
+            TestQueryGenerator._assert_intervals(
+                list(query_generator), num_items, interval
+            )
+
+    def test_fixed_rate(self) -> None:
+        sql_generator = DummySqlGenerator(num_items := 10)
+        interval = timedelta(seconds=1) / (queries_per_second := 2)
+
+        query_generator = QueryGenerator.with_fixed_rate(
+            sql_generator, queries_per_second
+        )
+
+        # Query generators can be iterated multiple times
+        for _ in range(NUM_ITERS):
+            TestQueryGenerator._assert_intervals(
+                list(query_generator), num_items, interval
+            )
+
+    @staticmethod
+    def _assert_intervals(
+        queries: list[Query], num_items: int, interval: timedelta
+    ) -> None:
+        assert [query.sql for query in queries] == [
+            SQL_TEMPLATE.format(i=i) for i in range(num_items)
+        ]
+
+        schedules: list[Once] = []
+        for query in queries:
+            assert isinstance(query.schedule, Once)
+            schedules.append(query.schedule)
+
+        intervals = list(after.at - before.at for before, after in pairwise(schedules))
+
+        assert intervals == list(repeat(interval, num_items - 1))
